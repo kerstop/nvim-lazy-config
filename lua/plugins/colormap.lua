622b80042468616f50e583efc24a4bc1710f7985
Connor Dalrymple <packetson2@gmail.com>
2024-09-11 09:36:13 -0500

    changed color scheme to ayu

---
+++ lua/plugins/colormap.lua
@@ -0,0 +1,13 @@
+return {
+  { "ellisonleao/gruvbox.nvim" },
+
+  { "Shatur/neovim-ayu" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "ayu",
+    },
+  },
+}
