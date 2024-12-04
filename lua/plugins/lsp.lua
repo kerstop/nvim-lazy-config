e6c254b954736acf98a6f5e57dd567b269f30cf0
kerstop <packetson2@gmail.com>
2024-12-04 15:03:33 -0600

    updates

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,3 +1,5 @@
+local nvim_lsp = require("lspconfig")
+
 return {
   {
     "neovim/nvim-lspconfig",
@@ -5,8 +7,24 @@ return {
       servers = {
         gopls = {},
         gdscript = {},
-        cssls = {},
-        denols = {},
+        cssls = {
+          root_dir = function()
+            vim.fs.root(0, ".git")
+          end,
+        },
+        denols = {
+          filetypes = { "typescript", "typescriptreact" },
+          root_dir = function(...)
+            return nvim_lsp.util.root_pattern("deno.jsonc", "deno.json")(...)
+          end,
+        },
+        vtsls = {
+          root_dir = function()
+            return not vim.fs.root(0, { "deno.json", "deno.jsonc" })
+              and vim.fs.root(0, { "tsconfig.json", "jsconfig.json", "package.json", ".git" })
+          end,
+          single_file_support = false,
+        },
       },
     },
   },
